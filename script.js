3871658023c55e5606ae36a29329ff228827536f
Joel Santos <joeltere9@gmail.com>
2022-08-22 13:43:46 -0300

    update 2

--- script.js
+++ script.js
@@ -35,3 +35,16 @@ ser.reveal('.home' ,{origin: 'rigth'})
 ser.reveal('.about' , {origin: 'left' , interval:600 , delay:400})
 
 ser.reveal('.superform', {origin: 'left' , delay:300})
+
+const logo = document.querySelector('.header-logo')
+const letters = logo.textContent.split("")
+
+logo.textContent = ""
+
+letters.forEach((letter) => {
+    logo.innerHTML += '<a class="letter" >' + letter + "</a>"
+    gsap.set('.letter' , {display: 'inline-block'})
+
+    gsap.fromTo('.letter' , {x: 200  } , {x: 0 , delay: 2, stagger: 0.05 , duration: 2})
+})
+
