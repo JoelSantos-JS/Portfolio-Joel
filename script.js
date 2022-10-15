3f207270dcc6cfb9e3ba29b53741ae642c1e37c6
Joel Santos <joeltere9@gmail.com>
2022-10-15 11:58:42 -0300

    update 2

--- script.js
+++ script.js
@@ -23,28 +23,3 @@ window.addEventListener('scroll', () =>{
 })
 
 
-const ser = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 3500,
-    delay: 400,
-})
-
-
-ser.reveal('.home' ,{origin: 'rigth'})
-ser.reveal('.about' , {origin: 'left' , interval:600 , delay:400})
-
-ser.reveal('.superform', {origin: 'left' , delay:300})
-
-const logo = document.querySelector('.header-logo')
-const letters = logo.textContent.split("")
-
-logo.textContent = ""
-
-letters.forEach((letter) => {
-    logo.innerHTML += '<a class="letter" >' + letter + "</a>"
-    gsap.set('.letter' , {display: 'inline-block'})
-
-    gsap.fromTo('.letter' , {x: 200  } , {x: 0 , delay: 2, stagger: 0.05 , duration: 2})
-})
-
